869c92113db6a87e14c9a51bd20ca3e084ca64e3
Tyler Roberts <dnbxna@gmail.com>
2016-12-22 18:17:43 -0500

    ngdatatable

--- src/client/app/dashboard/tables/table.component.ts
+++ src/client/app/dashboard/tables/table.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import {Http} from '@angular/http';
 
 
 @Component({
@@ -8,58 +8,93 @@ import { Component } from '@angular/core';
 	templateUrl: 'tables.component.html'
 })
 
-export class TableComponent {
+export class TableComponent implements OnInit {
 
-	date: any = new Date();
+public data: any[];
+    public filterQuery = '';
+    public rowsOnPage = 10;
+    public sortBy = 'email';
+    public sortOrder = 'asc';
 
-	newIssue: any = [{
-		repo: 'Career Builder',
-		issue: 'Can\'t login',
-		comment: ' Attack dog, run away and pretend to be victim with tail in the air',
-		opened: this.date.toDateString()
-	}, {
-		repo: 'Grocery App',
-		issue: 'Cafe c\'est non noir',
-		comment: 'J\'aime le cafè noir, avec la creme et du sugar. Mais, c\'est non noir a plus. ',
-		opened: this.date.toDateString()
-	}, {
-		repo: 'Grocery App ',
-		issue: 'Ou le pommes ',
-		comment: 'Je ne peux pas trouver les pommes, J\'ai regardé partout ',
-		opened: this.date.toDateString()
-	}, {
-		repo: 'Nec porta ',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: 'A claw then lick paws clean wash down prawns with a lap of carnation milk then retire to ',
-		opened: this.date.toDateString()
-	}, {
-		repo: 'lacus quam nunc eget',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: 'Lay on arms while you\'re using the keyboard eat prawns daintily',
-		opened: this.date.toDateString()
-	}, {
-		repo: 'Leo interdum lectus elementum',
-		issue: 'Lorem ipsum dolor sit amet.',
-		comment: '  Ignore the squirrels, you\'ll never catch them ',
-		opened: this.date.toDateString()
-	},];
+    constructor(private http: Http) {
+    }
+
+    ngOnInit(): void {
+        this.http.get('src/client/app/dashboard/tables/data.json')
+            .subscribe((data)=> {
+                setTimeout(()=> {
+                    this.data = data.json();
+                }, 2000);
+            });
+    }
+
+    public toInt(num: string) {
+        return +num;
+    }
+
+    public sortByWordLength = (a: any) => {
+        return a.city.length;
+    }
+
+    public remove = (item: any) => {
+        let index = this.data.indexOf(item);
+        if(index>-1) {
+            this.data.splice(index, 1);
+        }
+    }
+
+	// date: any = new Date();
+
+	// newIssue: any = [{
+	// 	repo: 'Career Builder',
+	// 	issue: 'Can\'t login',
+	// 	comment: ' Attack dog, run away and pretend to be victim with tail in the air',
+	// 	opened: this.date.toDateString()
+	// }, {
+	// 	repo: 'Grocery App',
+	// 	issue: 'Cafe c\'est non noir',
+	// 	comment: 'J\'aime le cafè noir, avec la creme et du sugar. Mais, c\'est non noir a plus. ',
+	// 	opened: this.date.toDateString()
+	// }, {
+	// 	repo: 'Grocery App ',
+	// 	issue: 'Ou le pommes ',
+	// 	comment: 'Je ne peux pas trouver les pommes, J\'ai regardé partout ',
+	// 	opened: this.date.toDateString()
+	// }, {
+	// 	repo: 'Nec porta ',
+	// 	issue: 'Lorem ipsum dolor sit amet.',
+	// 	comment: 'A claw then lick paws clean wash down prawns with a lap of carnation milk then retire to ',
+	// 	opened: this.date.toDateString()
+	// }, {
+	// 	repo: 'lacus quam nunc eget',
+	// 	issue: 'Lorem ipsum dolor sit amet.',
+	// 	comment: 'Lay on arms while you\'re using the keyboard eat prawns daintily',
+	// 	opened: this.date.toDateString()
+	// }, {
+	// 	repo: 'Leo interdum lectus elementum',
+	// 	issue: 'Lorem ipsum dolor sit amet.',
+	// 	comment: '  Ignore the squirrels, you\'ll never catch them ',
+	// 	opened: this.date.toDateString()
+	// },];
 
 
-	tableList: any = [
-		{
-			repo: 'Eu recusandae ',
-			issue: 'Lorem ipsum dolor sit amet.',
-			comment: ' Element convallis risus mauris nam magna wisi, hendrerit dui mauris tincidunt integer in a.',
-			opened: 'Dec 25'
-		}
-	];
+	// tableList: any = [
+	// 	{
+	// 		repo: 'Eu recusandae ',
+	// 		issue: 'Lorem ipsum dolor sit amet.',
+	// 		comment: ' Element convallis risus mauris nam magna wisi, hendrerit dui mauris tincidunt integer in a.',
+	// 		opened: 'Dec 25'
+	// 	}
+	// ];
 
-	private constructor() {
-		setInterval(() => {
-			this.tableList.push(this.newIssue[Math.floor(Math.random() * this.newIssue.length)]);
-		}, 10000);
-		setInterval(() => {
-			this.tableList.splice(Math.floor(Math.random() * this.tableList.length), 1);
-		}, 15000);
-	}
+	// public constructor() {
+	// 	setInterval(() => {
+	// 		this.tableList.push(this.newIssue[Math.floor(Math.random() * this.newIssue.length)]);
+	// 	}, 10000);
+	// 	setInterval(() => {
+	// 		this.tableList.splice(Math.floor(Math.random() * this.tableList.length), 1);
+	// 	}, 15000);
+	// }
+
+
 }
--- src/client/app/dashboard/tables/table.module.ts
+++ src/client/app/dashboard/tables/table.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import {CommonModule} from '@angular/common';
 import { TableComponent } from './table.component';
-
+import {DataTableModule} from 'angular2-datatable';
+import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, DataTableModule,FormsModule],
     declarations: [TableComponent],
     exports: [TableComponent]
 })
