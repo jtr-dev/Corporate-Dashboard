e78e79aea3f2b74794fd8c6418783d9d41d0211c
Tyler Roberts <Tyler Roberts>
2017-01-03 09:20:58 -0500

    chore: fix prod tasks

--- src/client/app/dashboard/home/home.component.ts
+++ src/client/app/dashboard/home/home.component.ts
@@ -1,4 +1,7 @@
 /// <reference path="../../../../../node_modules/@types/c3/index.d.ts" />
+/// <reference path="../../../../../node_modules/@types/d3/index.d.ts" />
+import * as d3 from 'd3';
+
 import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
 
